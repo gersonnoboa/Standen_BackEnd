7319945965d61e8e2e44bcb7c320648bcbcadabd
Gerson Noboa <gnoboa@protonmail.com>
2018-09-10 08:59:59 +0300

    Added useful packages

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const debug = require("debug")("standen")
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 app.use(express.json());
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+}
 
 app.get("/", (req, res) => {
-    res.send("Standen is alive");
+  res.send("Standen is alive");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`);
 });
