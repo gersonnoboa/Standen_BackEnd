15345f7364307b08303c07b5bcd3cdd4b33e143a
Gerson Noboa <gnoboa@protonmail.com>
2018-09-16 19:13:27 +0300

    Added team get and post

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const mongoose = require("mongoose")
-const schema = mongoose.Schema({
+const mongoose = require("mongoose");
+const tournamentSchema = mongoose.Schema({
     name: String
 });
-const Tournament = mongoose.model("Tournament", schema);
+const Tournament = mongoose.model("Tournament", tournamentSchema);
+
+const teamSchema = mongoose.Schema({
+    name: String,
+    tournament: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tournament"
+    }
+});
+const Team = mongoose.model("Team", teamSchema);
 
 router.get("/", async (req, res) => {
     const tournaments = await Tournament.find();
     res.send(tournaments);
+});
+
+router.get("/:id/teams", async (req, res) => {
+    const teams = await Team.find({ tournament: req.params.id });
+    res.send(teams);
 });
 
 router.post("/", async (req, res) => {
@@ -17,6 +31,15 @@ router.post("/", async (req, res) => {
         name: req.body.name
     });
     const result = await tournament.save();
+    res.send(result);
+});
+
+router.post("/:id/teams", async (req, res) => {
+    const team = new Team({
+        name: req.body.name,
+        tournament: req.body.tournament
+    });
+    const result = await team.save();
     res.send(result);
 });
 
